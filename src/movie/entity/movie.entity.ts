979efc29c91e49a962a21153f91513b60faa8396
Cjungwo <jwoo516@naver.com>
2025-03-24 12:06:09 +1100

    #5-entity embedding & inheritance theory

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -7,17 +7,7 @@ import {
   VersionColumn,
 } from 'typeorm';
 
-@Entity()
-export class Movie {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  title: string;
-
-  @Column()
-  genre: string;
-
+export class BaseEntity {
   @CreateDateColumn()
   createdAt: Date;
 
@@ -27,3 +17,46 @@ export class Movie {
   @VersionColumn()
   version: number;
 }
+
+// @Entity()
+// @TableInheritance({
+//   column: {
+//     type: 'varchar',
+//     name: 'type',
+//   },
+// })
+// export class Content extends BaseEntity {
+//   @PrimaryGeneratedColumn()
+//   id: number;
+
+//   @Column()
+//   title: string;
+
+//   @Column()
+//   genre: string;
+// }
+
+// movie / series -> Content
+// runtime / seriesCount
+
+@Entity()
+export class Movie extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  genre: string;
+
+  // Embedded
+  // @Column(() => BaseEntity)
+  // base: BaseEntity;
+}
+
+// @ChildEntity()
+// export class Series extends Content {
+//   @Column()
+//   seriesCount: number;
+// }
