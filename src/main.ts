d10cf9118f98efbb27b3de533dfeb8785cf52b55
cjungwo <jwoo516@naver.com>
2025-04-18 19:52:49 +1000

    #29-logger add logger use cases

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
 import { logger } from './common/middleware/logger.middleware';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['verbose'],
+  });
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.use(logger);
   app.useGlobalPipes(
     new ValidationPipe({
